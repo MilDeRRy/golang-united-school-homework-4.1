5141d0eeef3b1179a0e6a30fa5b5c88952a3193f
MilDeRRy <bane009@yandex.ru>
2022-06-27 16:03:30 +0400

    test 1

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,28 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
+	for i := len(input) - 1; i >= 0; i-- {
+		res := (string(input[i]))
+		output += res
+	}
 	return output
 }
+
+/*package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	s := "Hello world!"
+	grim := ReverseString(s)
+	fmt.Println(grim)
+}
+func ReverseString(input string) (output string) {
+	for i := len(input) - 1; i >= 0; i-- {
+		res := (string(input[i]))
+		output += res
+	}
+	return output
+}*/
